430cc9ed522bae5c033fb53712067b66df2341ec
Rustem Saitkulov <rustem.saitkulov@dz.ru>
2015-11-30 15:48:43 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ##Set 1
 
-[SET 1.1] (TestCTCI/src/main/java/wzhishen/additional/set1/Q1.java) You've got an array of ints 2,2,3,3,4,5,5... Find an element without a pair.
+[SET 1.1] (test/atetc/set1/Q1.java) You've got an array of ints 2,2,3,3,4,5,5... Find an element without a pair.
---
+++ src/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a1 = -256464896;
+        int a2 = -16;
+        System.out.println(Integer.toBinaryString(a1));
+
+        int b = a1 >> 29;
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(b);
+
+    }
+}
