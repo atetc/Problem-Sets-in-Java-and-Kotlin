0bbcd15d9df6ce3278e56df1b32a9a878fbfcd1b
Rustem Saitkulov <srmsam@yandex.ru>
2015-12-11 17:02:58 +0300

    Binary tree problem 1 added

---
+++ src/java/atetc/other/linkedlists/Q2.java
@@ -0,0 +1,7 @@
+package atetc.other.linkedlists;
+
+/**
+ * Given a singly connected linked list, find the largest palindrome in the list in O(1) space.
+ */
+public class Q2 {
+}
---
+++ src/java/atetc/other/trees/binary/Q1.java
@@ -0,0 +1,19 @@
+package atetc.other.trees.binary;
+
+/**
+ * From Akvelon set 26
+ *
+ * Given a binary tree print it in inward spiral order i.e first print level 1, then level n, then level 2, then n-1 and so on.
+ * For Ex:
+ * 1
+ * 2 3
+ * 4 5 6 7
+ * 8 9 10 11 12 13 14 15
+ *
+ * Print- 1 15 14 13 12 11 10 9 8 2 3 7 6 5 4
+ *
+ * Extend the algorithm to n-ary tree.
+ */
+public class Q1 {
+
+}
